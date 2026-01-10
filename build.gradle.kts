fd6aef60a82a037cca66184b42bafe9c63c037bf
MrLarkyy_ <studiotadikl@gmail.com>
2026-01-10 05:11:50 +0100

    Updated dotEnv plugin usage

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,13 @@ plugins {
     kotlin("jvm") version "2.3.0"
     id("com.gradleup.shadow") version "9.3.1"
     id("io.papermc.paperweight.userdev") version "2.0.0-beta.19"
-    id("co.uzzu.dotenv.gradle") version "4.0.0"
+    id("co.uzzu.dotenv.gradle") version "4.0.0" apply false
     `maven-publish`
     java
+}
+
+if (project == rootProject) {
+    apply(plugin = "co.uzzu.dotenv.gradle")
 }
 
 group = "gg.aquatic.replace"
